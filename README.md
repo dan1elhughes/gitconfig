5731fea9f1886c26e6974329381ef543df50bbd6
Dan Hughes <danhughes@monzo.com>
2020-08-26 11:33:37 +0100

    Remove variables, simplify

--- README.md
+++ README.md
@@ -4,14 +4,6 @@ Template and basic installer for my git config file.
 
 ## Usage
 
-Create `.env` and fill in:
+Run `bash install.sh` to symlink the config into `~/.gitconfig`.
 
-```
-GIT_EMAIL="you@email.com"
-GIT_NAME="Your Name"
-GIT_SIGNING_KEY="Y0URS1GN1NGK3Y"
-```
-
-Then run `bash install.sh` to template out the config into `~/.gitconfig`.
-
-*Note that this is a dumb installer - back up existing config first*
+_Note that this is a dumb installer - back up existing config first_
--- install.sh
+++ install.sh
@@ -1,28 +1,3 @@
 #!/usr/bin/env bash
 
-. .env
-
-# Set the comment variables if the email/name/key variables are empty.
-[ -z "$GIT_EMAIL" ] && _if_email=';'
-[ -z "$GIT_NAME" ] && _if_name=';'
-[ -z "$GIT_SIGNING_KEY" ] && _if_key=';'
-[ -z "$GIT_EDITOR" ] && _if_editor=';'
-
-# Run the template through sed as a lazy templating language, and output to the final ~/.gitconfig location
-cat "./gitconfig" | \
-sed -e "s/{{ _if_email }}/$_if_email/g" | \
-sed -e "s/{{ GIT_EMAIL }}/$GIT_EMAIL/g" | \
-sed -e "s/{{ _if_name }}/$_if_name/g" | \
-sed -e "s/{{ GIT_NAME }}/$GIT_NAME/g" | \
-sed -e "s/{{ _if_key }}/$_if_key/g" | \
-sed -e "s/{{ GIT_SIGNING_KEY }}/$GIT_SIGNING_KEY/g" | \
-sed -e "s/{{ _if_editor }}/$_if_editor/g" | \
-sed -e "s/{{ GIT_EDITOR }}/$GIT_EDITOR/g" \
-> "$HOME/.gitconfig"
-
-echo ":: $HOME/.gitconfig"
-
-if [ -d "$HOME/.oh-my-zsh/custom" ]; then
-	cp "./aliases.zsh" "$HOME/.oh-my-zsh/custom/git.zsh"
-	echo ":: $HOME/.oh-my-zsh/custom/git.zsh"
-fi
+ln -sfv $PWD/gitconfig ~/.gitconfig
